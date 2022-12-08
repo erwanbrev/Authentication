d89ce13bb3b1a579b12babf175feb4f9db88d9ab
Erwan Brévault <devbzh35@gmail.com>
2022-12-08 20:01:33 +0100

    update admin panel css

--- pages/view/admin-panel-create.php
+++ pages/view/admin-panel-create.php
@@ -8,50 +8,40 @@ require_once "../controller/create-users.php";
 <head>
     <meta charset="UTF-8">
     <title>Create Record</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        .wrapper {
-            width: 600px;
-            margin: 0 auto;
-        }
-    </style>
+    <link rel="stylesheet" href="../model/style.css">
 </head>
 
 <body>
-    <div class="wrapper">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <h2 class="mt-5">Create Record</h2>
-                    <p>Please fill this form and submit to add employee record to the database.</p>
-                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                        <div class="form-group">
-                            <label>Username</label>
-                            <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
-                            <span class="invalid-feedback"><?php echo $username_err; ?></span>
-                        </div>
-                        <div class="form-group">
-                            <label>Mail</label>
-                            <input name="mail" class="form-control <?php echo (!empty($mail_err)) ? 'is-invalid' : ''; ?>"><?php echo $mail; ?></input>
-                            <span class="invalid-feedback"><?php echo $mail_err; ?></span>
-                        </div>
-                        <div class="form-group">
-                            <label>Password</label>
-                            <input name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>"><?php echo $password; ?></input>
-                            <span class="invalid-feedback"><?php echo $password_err; ?></span>
-                        </div>
-                        <div class="form-group">
-                            <label>Role</label>
-                            <input type="text" name="role" class="form-control <?php echo (!empty($role_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $role; ?>">
-                            <span class="invalid-feedback"><?php echo $role_err; ?></span>
-                        </div>
-                        <input type="submit" class="btn btn-primary" value="Submit">
-                        <a href="../controller/admin-panel.php" class="btn btn-secondary ml-2">Cancel</a>
-                    </form>
-                </div>
+    <h2>Create Record</h2>
+    <p>Please fill this form and submit to add user record to the database.</p>
+    <section class="form-container">
+        <form class="form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div>
+                <label>Username</label>
+                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
             </div>
-        </div>
-    </div>
+            <div>
+                <label>Mail</label>
+                <input name="mail" class="form-control <?php echo (!empty($mail_err)) ? 'is-invalid' : ''; ?>"><?php echo $mail; ?></input>
+                <span class="invalid-feedback"><?php echo $mail_err; ?></span>
+            </div>
+            <div>
+                <label>Password</label>
+                <input name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>"><?php echo $password; ?></input>
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div>
+                <label>Role</label>
+                <input type="text" name="role" class="form-control <?php echo (!empty($role_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $role; ?>">
+                <span class="invalid-feedback"><?php echo $role_err; ?></span>
+            </div>
+            <button class="form-btn" type="submit" value="Submit">Submit</button>
+            <button class="form-btn" value="Cancel">
+                <a href="../controller/admin-panel.php">Cancel</a>
+            </button>
+        </form>
+    </section>
 </body>
 
 </html>
